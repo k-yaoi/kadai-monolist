7051371ac9e3030d80c698fc76d4821f16fca1c7
k-yaoi <kaoru.yaoi@gmail.com>
2018-05-21 14:28:06 +0000

    want

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-  end
+    @items = @user.items.uniq
+    @count_want = @user.want_items.count  end
 
   def new
     @user = User.new
